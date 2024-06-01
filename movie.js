7816f5684d10643e547a7f732b84720de63653a6
sevketugurel <ugurelsevket@gmail.com>
2024-06-01 11:28:01 +0300

    Fronted ve backend baağlantısı Yapıldı.

--- movie.js
+++ movie.js
@@ -1,51 +1,131 @@
+const url = new URL(window.location.href);
+const movieTitle = url.searchParams.get("title");
+const movieId = url.searchParams.get('id');
+
 const APILINK = "http://localhost:8000/api/v1/reviews/";
 
 const main = document.getElementById("section");
+const title = document.getElementById("title");
 
+title.innerText = movieTitle;
 
-returnMovies(APILINK);
+const div_new = document.createElement('div');
+div_new.innerHTML = `
+  <div class="row">
+    <div class="column">
+      <div class="card">
+          New Review
+          <p><strong>Review: </strong>
+            <input type="text" id="new_review" value="">
+          </p>
+          <p><strong>User: </strong>
+            <input type="text" id="new_user" value="">
+          </p>
+          <p><a href="#" onclick="saveReviewNew('new_review', 'new_user')">💾</a></p>
+      </div>
+    </div>
+  </div>
+`
+main.appendChild(div_new)
 
-function returnMovies(url) {
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data.results);
-      const div_row = document.createElement('div');
-      div_row.setAttribute('class', 'row');
+returnReviews(APILINK);
 
-      data.results.forEach((element, index) => {
+function returnReviews(url) {
+  fetch(url + "movie/" + movieId).then(res => res.json())
+    .then(function(data) {
+      console.log(data);
+      main.innerHTML = div_new.outerHTML; // Eski incelemeleri temizle
+      data.forEach(review => {
         const div_card = document.createElement('div');
-        div_card.setAttribute('class', 'card');
-
-        const div_column = document.createElement('div');
-        div_column.setAttribute('class', 'column');
-
-        const image = document.createElement('img');
-        image.setAttribute('class', 'thumbnail');
-        image.setAttribute('id', 'image');
-
-        const title = document.createElement('h3');
-        title.setAttribute('id', 'title');
-
-        const center = document.createElement('center');
-
-        title.innerHTML = `${element.title}`;
-        image.src = IMG_PATH + element.poster_path;
-
-        center.appendChild(image);
-        div_card.appendChild(center);
-        div_card.appendChild(title);
-        div_column.appendChild(div_card);
-        div_row.appendChild(div_column);
-
-        if ((index + 1) % 4 === 0) {
-          main.appendChild(div_row.cloneNode(true));
-          div_row.innerHTML = '';
-        }
+        div_card.innerHTML = `
+          <div class="row">
+            <div class="column">
+              <div class="card" id="${review._id}">
+                <p><strong>Review: </strong>${review.review}</p>
+                <p><strong>User: </strong>${review.user}</p>
+                <p><a href="#"onclick="editReview('${review._id}','${review.review}', '${review.user}')">Save</a> <a href="#" onclick="deleteReview('${review._id}')">🗑</a></p>
+              </div>
+            </div>
+          </div>
+        `
+        main.appendChild(div_card);
       });
+    });
+}
 
-      if (div_row.innerHTML !== '') {
-        main.appendChild(div_row);
-      }
+function editReview(id, review, user) {
+  const element = document.getElementById(id);
+  const reviewInputId = "review" + id;
+  const userInputId = "user" + id;
+
+  element.innerHTML = `
+    <p><strong>Review: </strong>
+      <input type="text" id="${reviewInputId}" value="${review}">
+    </p>
+    <p><strong>User: </strong>
+      <input type="text" id="${userInputId}" value="${user}">
+    </p>
+    <p><a href="#" onclick="saveReview('${reviewInputId}', '${userInputId}', '${id}')">💾</a></p>
+  `;
+}
+function saveReviewNew(reviewInputId, userInputId) {
+  const review = document.getElementById(reviewInputId).value;
+  const user = document.getElementById(userInputId).value;
+
+  fetch(APILINK + "new", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"user": user, "review": review, "movieId": movieId})
+  }).then(res => res.json())
+    .then(res => {
+      console.log(res)
+      location.reload();
+    });
+}
+
+
+function saveReview(reviewInputId, userInputId, id = "") {
+  const review = document.getElementById(reviewInputId).value;
+  const user = document.getElementById(userInputId).value;
+
+  if (id) {
+    fetch(APILINK + id, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "user": user, "review": review })
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res);
+        returnReviews(APILINK); // İncelemeleri güncelle
+      });
+  } else {
+    fetch(APILINK + "new", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "user": user, "review": review, "movieId": movieId })
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res);
+        returnReviews(APILINK); // İncelemeleri güncelle
+      });
+  }
+}
+
+function deleteReview(id) {
+  fetch(APILINK + id, {
+    method: 'DELETE'
+  }).then(res => res.json())
+    .then(res => {
+      console.log(res);
+      returnReviews(APILINK); // İncelemeleri güncelle
     });
 }
--- script.js
+++ script.js
@@ -1,66 +1,58 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=&page=2";
+const apiKey = 'b1b097cebbc59681982a6e8564267e43';
+const APILINK = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=&query=";
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${apiKey}&query=`;
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
-returnMovies(APILINK);
+returnMovies(APILINK)
+function returnMovies(url){
+  fetch(url).then(res => res.json())
+    .then(function(data){
+      console.log(data.results);
+      data.results.forEach(element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
 
-function returnMovies(url) {
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-          console.log(data.results);
-          const div_row = document.createElement('div');
-          div_row.setAttribute('class', 'row');
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
 
-          data.results.forEach((element, index) => {
-              const div_card = document.createElement('div');
-              div_card.setAttribute('class', 'card');
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
 
-              const div_column = document.createElement('div');
-              div_column.setAttribute('class', 'column');
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
 
-              const image = document.createElement('img');
-              image.setAttribute('class', 'thumbnail');
-              image.setAttribute('id', 'image');
+        const title = document.createElement('h3');
+        title.setAttribute('id', 'title');
 
-              const title = document.createElement('h3');
-              title.setAttribute('id', 'title');
+        const center = document.createElement('center');
 
-              const center = document.createElement('center');
+        title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}" class="button">Reviews</a>`;
+        image.src = IMG_PATH + element.poster_path;
 
-            title.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">Reviews</a>`;
-            image.src = IMG_PATH + element.poster_path;
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
 
-              center.appendChild(image);
-              div_card.appendChild(center);
-              div_card.appendChild(title);
-              div_column.appendChild(div_card);
-              div_row.appendChild(div_column);
-
-              if ((index + 1) % 4 === 0) {
-                  main.appendChild(div_row.cloneNode(true));
-                  div_row.innerHTML = '';
-              }
-          });
-
-          if (div_row.innerHTML !== '') {
-              main.appendChild(div_row);
-          }
+        main.appendChild(div_row);
       });
+    });
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    main.innerHTML = '';
+  e.preventDefault();
+  main.innerHTML = '';
 
-    const searchItem = search.value;
+  const searchItem = search.value;
 
-    if (searchItem) {
-        returnMovies(SEARCHAPI + searchItem);
-        search.value = '';
-    }
+  if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
+    search.value = "";
+  }
 });
