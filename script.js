7c340e2d43a115c93336c521d2a0f1a2125b3a83
sevketugurel <ugurelsevket@gmail.com>
2024-06-04 20:34:13 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const apiKey ="b1b097cebbc59681982a6e8564267e43";
-const APILINK = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`;
+import * as api from './config.js';
+
+const APILINK = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key="${api.APIKEY}"`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 
